f13e594cb7af7bc19fb2a9d8aba599385e288a31
ante <ante.kotarac@gmail.com>
2021-12-04 19:12:08 +0100

    week 2 kmer indexing

--- genomic/week02/skip.py
+++ genomic/week02/skip.py
@@ -1,2 +1,27 @@
-# CCGGTGTTTGAC
-#  GATTATT
+import bisect
+
+class Index:
+    def __init__(self, t, k):
+        self.k = k
+        self.index = []
+        for i in range(len(t) + 1 - k):
+            self.index.append((t[i:i+k], i))
+        self.index.sort()
+    
+    def query(self, p):
+        kmer = p[:self.k]
+        
+        start = bisect.bisect_left(self.index, (kmer, -1))
+        
+        result = []
+        
+        for i in range(start, len(self.index)):
+            if self.index[i][0] == kmer:
+                result.append(i)
+            else:
+                break
+        
+        return result
+
+index = Index('AGAGACGATA', 3)
+print(index.query('AGA'))
